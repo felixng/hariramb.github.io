41ae53f015544372d260a476409add3b08a6f8c4
hariramb <hariram.mit25@gmail.com>
2016-10-28 11:45:26 -0700

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,17 @@
+var request;
+if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+  request = new XMLHttpRequest();
+} else if (window.ActiveXObject) { // IE
+  try {
+    request = new ActiveXObject('Msxml2.XMLHTTP');
+  } 
+  catch (e) {
+    try {
+      request = new ActiveXObject('Microsoft.XMLHTTP');
+    } 
+    catch (e) {}
+  }
+}
+
+request.open('GET', 'https://127.0.0.1:8000', true);
+request.send(null);
