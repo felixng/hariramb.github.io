b8c48dabbb74f8a3f9e31d89a50f169ce1d643ec
hariramb <hariram.mit25@gmail.com>
2016-10-31 11:28:20 -0700

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
 var x = new XMLHttpRequest();
-x.open("GET", "http://127.0.0.1:8080/job/d2ef3763041956dde500eff003d090e05a71feb7/ws/results.json");
+x.open("GET", "http://127.0.0.1:9000/temp.txt");
 x.send();
 document.write(x.responseText);
